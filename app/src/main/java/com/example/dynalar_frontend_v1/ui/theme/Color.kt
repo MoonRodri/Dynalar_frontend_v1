cfa241db7d53eae8c3455a66ef18488d2241410f
Takkun <rodrimd231@gmail.com>
2026-02-26 16:06:21 +0100

    Color update

--- app/src/main/java/com/example/dynalar_frontend_v1/ui/theme/Color.kt
+++ app/src/main/java/com/example/dynalar_frontend_v1/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.dynalar_frontend_v1.ui.theme
+package com.example.dynalar_frontend.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Colores Del figma
+
+val ButtonPrimary = Color(0xFF4A6D8C) // Color primario botones (hex #4A6D8C)
+val SkyBlue = Color(0xFF23B9E7) // Color secundario Azul Celeste (hex #23b9e7)
+val SecondaryBg = Color(0xFFADD4D9) // Color BG secundario (hex #ADD4D9)
